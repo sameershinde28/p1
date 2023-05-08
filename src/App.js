1a4d152eb0c35f8dce77249624ded91f4b2e2a14
sameer <sameershinde@gmail.com>
2023-05-08 12:26:57 +0530

    component calling in another compo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
- export default function App(){
-  return(
+
+function Heading() {
+  return (
+    <h1>Heading</h1>
+  )
+}
+
+export default function App() {
+  return (
     <div>
-    <h1>hello react.... how r u??????</h1>
+      <Heading />
+      <h1>hello react.... how r u??????</h1>
     </div>
   )
- }
+}
